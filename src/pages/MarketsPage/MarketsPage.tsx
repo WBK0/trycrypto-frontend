42b85b0bc3b35cd5f0ca4d0e5d49718045c28cea
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-04-05 18:13:44 +0200

    Table market update

--- src/pages/MarketsPage/MarketsPage.tsx
+++ src/pages/MarketsPage/MarketsPage.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../../layout/Layout/Layout";
+import api from './../../services/api';
 import {
   createColumnHelper,
   flexRender,
@@ -9,31 +10,12 @@ import {
 
 type Person = {
   pair: string
-  price: number
-  change: number
-  visits: number
-  status: string
-  progress: number
+  lastPrice: string
+  percentChange: string
+  highPrice: string
+  lowPrice: string
+  volume: string
 }
-
-const defaultData: Person[] = [
-  {
-    pair: 'tanner',
-    price: 421.23,
-    change: 24,
-    visits: 100,
-    status: 'In Relationship',
-    progress: 50,
-  },
-  {
-    pair: 'tandy',
-    price: 432.21312,
-    change: 40,
-    visits: 40,
-    status: 'Single',
-    progress: 80,
-  },
-]
 
 const columnHelper = createColumnHelper<Person>()
 
@@ -41,36 +23,61 @@ const columns = [
   columnHelper.accessor('pair', {
     cell: info => info.getValue(),
     header: () => <span>Name</span>,
-    footer: info => info.column.id,
   }),
-  columnHelper.accessor('price', {
+  columnHelper.accessor('lastPrice', {
     id: 'lastName',
-    cell: info => info.getValue(),
+    cell: info => {
+      const value = parseFloat(info.getValue());
+      return value > 1 ? value.toFixed(2) : value.toFixed(4);
+    },
     header: () => <span>Price</span>,
-    footer: info => info.column.id,
   }),
-  columnHelper.accessor('change', {
+  columnHelper.accessor('percentChange', {
     header: () => 'Price change',
-    cell: info => info.getValue(),
-    footer: info => info.column.id,
+    cell: info => {
+      const value = parseFloat(info.getValue());
+      return value.toFixed(2) + "%";
+    },
   }),
-  columnHelper.accessor('visits', {
-    header: () => <span>Visits</span>,
-    footer: info => info.column.id,
+  columnHelper.accessor('highPrice', {
+    header: () => <span>24h High</span>,
+    cell: info => {
+      const value = parseFloat(info.getValue());
+      return value > 1 ? value.toFixed(2) : value.toFixed(4);
+    },
   }),
-  columnHelper.accessor('status', {
-    header: 'Status',
-    footer: info => info.column.id,
+  columnHelper.accessor('lowPrice', {
+    header: '24h Low',
+    cell: info => {
+      const value = parseFloat(info.getValue());
+      return value > 1 ? value.toFixed(2) : value.toFixed(4);
+    },
   }),
-  columnHelper.accessor('progress', {
-    header: 'Profile Progress',
-    footer: info => info.column.id,
+  columnHelper.accessor('volume', {
+    header: '24h Volume',
+    cell: info => {
+      const value = parseFloat(info.getValue());
+      return (value / 1000000).toFixed(2) + "M";
+    },
   }),
 ]
 
 const MarketsPage = () => {
-  const [data, setData] = useState(() => [...defaultData])
+  const [data, setData] = useState(() => [])
 
+  useEffect(() => {
+    api.get("/data")
+    .then((response) => {
+      const res = response.data.spot;
+      let res2: any = []
+      res2 = Object.keys(res).map(key => res[key]);  
+      setData(res2) 
+    })
+
+    
+    
+  },[])
+ 
   const table = useReactTable({
     data,
     columns,
@@ -84,16 +91,12 @@ const MarketsPage = () => {
         <thead>
           {table.getHeaderGroups().map(headerGroup => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h Change</th>
+              <th>24h High</th>
+              <th>24h Low</th>
+              <th>24h Volume</th>
             </tr>
           ))}
         </thead>
