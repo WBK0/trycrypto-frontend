b15f478dc96d39ff207a6d72e4b39ea82d9e6447
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-04-15 17:02:08 +0200

    Spot trading site changes

--- src/pages/TradingPages/SpotPage/SpotPage.tsx
+++ src/pages/TradingPages/SpotPage/SpotPage.tsx
@@ -1,11 +1,109 @@
 import { useParams } from "react-router-dom";
 import TradingLayout from "../../../layout/TradingLayout/TradingLayout";
 import styles from './spotPage.module.css';
-import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
+import { useEffect, useRef, useState } from "react";
+
+interface TradingView {
+  widget: (options: any) => any;
+}
+
+declare global {
+  interface Window {
+    TradingView: TradingView;
+  }
+}
 
 const SpotPage: React.FC = () => {
+  const [socket, setSocket] = useState<any>(null);
+  const [data, setData] = useState<any>({
+    c: null,
+p: null,
+  });
+  useEffect(() => {
+    const newSocket = new WebSocket('wss://stream.binance.com/ws/' + symbol + '@ticker');
+
+    newSocket.addEventListener('open', () => {
+      console.log('Connected to WebSocket');
+    });
+
+    newSocket.addEventListener('message', (event) => {
+      setData(JSON.parse(event.data));
+    });
+
+    newSocket.addEventListener('close', () => {
+      console.log('Disconnected from WebSocket');
+
+      // Optionally, try to reconnect to the WebSocket after a short delay
+      setTimeout(() => {
+        setSocket(null);
+      }, 5000);
+    });
+
+    setSocket(newSocket);
+
+    return () => {
+      if (socket) {
+        socket.close();
+      }
+    };
+  }, []);
+
 
   const {symbol} = useParams()
+  console.log(data.c);
+  let tvScriptLoadingPromise: Promise<Event>;
+const onLoadScriptRef = useRef<(() => void) | null>(null);
+
+useEffect(() => {
+  onLoadScriptRef.current = createWidget;
+
+  if (!tvScriptLoadingPromise) {
+    tvScriptLoadingPromise = new Promise<Event>((resolve) => {
+      const script = document.createElement('script');
+      script.id = 'tradingview-widget-loading-script';
+      script.src = 'https://s3.tradingview.com/tv.js';
+      script.type = 'text/javascript';
+      script.onload = resolve;
+
+      document.head.appendChild(script);
+    });
+  }
+
+  tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+  return () => {
+    onLoadScriptRef.current = null;
+  };
+
+  function createWidget() {
+    if (document.getElementById('tradingview_33c1c') && typeof window.TradingView === 'object') {
+      const widgetOptions = {
+        autosize: true,
+        symbol: 'BINANCE:' + symbol,
+        interval: 'D',
+        timezone: 'Etc/UTC',
+        theme: 'dark',
+        style: '1',
+        locale: 'en',
+        toolbar_bg: '#f1f3f6',
+        enable_publishing: false,
+        allow_symbol_change: false,
+        container_id: 'tradingview_33c1c',
+        
+      };
+      const widget = new (window as any).TradingView.widget(widgetOptions);
+      // dodanie atrybutu SameSite do cookies, które przesyłane są przez TradingView
+    const cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookieParts = cookies[i].split('=');
+      const cookieName = cookieParts[0];
+      const cookieValue = cookieParts[1];
+      document.cookie = `${cookieName}=${cookieValue}; SameSite=Lax`;
+    }
+    }
+  
+  }
+}, []);
 
   return(
     <TradingLayout>
@@ -22,27 +120,27 @@ const SpotPage: React.FC = () => {
                 <div className="row align-items-center h-100">
                   <div className="d-flex flex-column align-items-start col-4 col-lg-2">
                     <p className={`mb-0 ${styles.header}`}>PRICE</p>
-                    <p className={`mb-0 ${styles.info}`}>1321.03$</p>
+                    <p className={`mb-0 ${styles.info}`}>{Number(data.c).toFixed(2)}$</p>
                   </div>
                   <div className="d-flex flex-column align-items-start col-4 col-lg-2">
                     <p className={`mb-0 ${styles.header}`}>24H CHANGE</p>
-                    <p className={`mb-0 ${styles.info}`}>213.01$ +13%</p>
-                  </div>
-                  <div className="d-flex flex-column align-items-start col-4 col-lg-2">
-                    <p className={`mb-0 ${styles.header}`}>OPEN PRICE</p>
-                    <p className={`mb-0 ${styles.info}`}>1223.21$</p>
+                    <p className={`mb-0 ${styles.info}`}>{Number(data.p).toFixed(2)}$ {Number(data.P).toFixed(2)}%</p>
                   </div>
                   <div className="d-flex flex-column align-items-start col-4 col-lg-2">
                     <p className={`mb-0 ${styles.header}`}>24H HIGH</p>
-                    <p className={`mb-0 ${styles.info}`}>1333.21$</p>
+                    <p className={`mb-0 ${styles.info}`}>{Number(data.h).toFixed(2)}$</p>
                   </div>
                   <div className="d-flex flex-column align-items-start col-4 col-lg-2">
                     <p className={`mb-0 ${styles.header}`}>24H LOW</p>
-                    <p className={`mb-0 ${styles.info}`}>1123.49$</p>
+                    <p className={`mb-0 ${styles.info}`}>{Number(data.l).toFixed(2)}$</p>
+                  </div>
+                  <div className="d-flex flex-column align-items-start col-4 col-lg-2">
+                    <p className={`mb-0 ${styles.header}`}>OPEN PRICE</p>
+                    <p className={`mb-0 ${styles.info}`}>{Number(data.o).toFixed(2)}$</p>
                   </div>
                   <div className="d-flex flex-column align-items-start col-4 col-lg-2">
                     <p className={`mb-0 ${styles.header}`}>24H VOLUME</p>
-                    <p className={`mb-0 ${styles.info}`}>12.3M</p>
+                    <p className={`mb-0 ${styles.info}`}>{Number(data.q * 0.000001).toFixed(2)}M</p>
                   </div>
                   
                 </div>
@@ -50,7 +148,12 @@ const SpotPage: React.FC = () => {
               </div>
               <div className="row me-0 ms-0 pe-0 ps-0">
                 <div className={styles.chartContainer}>
-                  <AdvancedRealTimeChart theme="dark" autosize></AdvancedRealTimeChart>
+                  <div className='tradingview-widget-container h-100'>
+                    <div id='tradingview_33c1c' className="h-100"/>
+                    <div className="tradingview-widget-copyright">
+                      <a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span className="blue-text">{symbol?.toUpperCase()} chart</span></a> by TradingView
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
