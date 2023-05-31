b7b15c2db3a6c10f226875f1827f3ad92ba6c57a
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-05-31 23:26:09 +0200

    Add symbol change in futures market

--- src/pages/TradingPages/FuturesPage/components/symbol/Symbol.tsx
+++ src/pages/TradingPages/FuturesPage/components/symbol/Symbol.tsx
@@ -1,14 +1,60 @@
-import { Wrapper } from "./symbol.styles";
+import { useEffect, useState } from "react";
+import { Change, CurrenciesWrapper, DataWrapper, Name, Price, Wrapper } from "./symbol.styles";
+import getData from "../../../../../components/Markets/services/getData";
+import { MarketData } from "../../../../../components/Markets/interfaces/interfaces";
 
 interface ISymbol{
   symbol?: string;
 }
 
 const Symbol: React.FC<ISymbol> = ({ symbol }) => {
+  const [data, setData] = useState<MarketData[] | []>([]);
+
+  useEffect(() => {
+    const fetchData = async (market : string) => {
+      const data = await getData(market);
+      setData(prevData => {
+        const updatedData = data.map((item: {lastPrice: string}, index) => ({
+          ...item,
+          color: item.lastPrice > prevData[index]?.lastPrice ? 'rgb(7, 119, 3)' :  item.lastPrice < prevData[index]?.lastPrice ? 'rgb(119, 3, 3)' : prevData[index]?.color || 'rgb(200, 200, 200)',
+        }));
+        return updatedData as MarketData[];
+      });
+    };
+
+    fetchData('futures');
+    // Setting up an interval to fetch data
+    const interval = setInterval(() => fetchData('futures'), 6000);
+
+    // Clearing the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
   return(
-    <Wrapper>
-      {symbol?.replace('usdt', '/usdt').toUpperCase()}
-    </Wrapper>
+    <>
+      <Wrapper>
+        {symbol?.replace('usdt', '/usdt').toUpperCase()}
+        <CurrenciesWrapper>
+          {data.map((item) => {
+            return(
+              <DataWrapper>
+                <Name>
+                  {item.pair}
+                </Name>
+                <Change>
+                  {Number(item.percentChange).toFixed(2)}%
+                </Change>
+                <Price>
+                  {Number(item.lastPrice).toFixed(4)}
+                </Price>
+              </DataWrapper>
+            )
+          })}
+        </CurrenciesWrapper>
+      </Wrapper>
+      
+    </>
+   
   )
 }
 
--- src/pages/TradingPages/FuturesPage/components/symbol/symbol.styles.ts
+++ src/pages/TradingPages/FuturesPage/components/symbol/symbol.styles.ts
@@ -4,9 +4,11 @@ export const Wrapper = styled.div`
   width: 100%;
   height: 60px;
   color: white;
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
   font-size: 20px;
   font-weight: 500;
   border-bottom: 10px solid var(--theme-dark);
@@ -16,4 +18,50 @@ export const Wrapper = styled.div`
   @media screen and (min-width: 1400px){
     border-right: none;
   }
+  
+`
+
+export const CurrenciesWrapper = styled.div`
+  display: none;
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  margin-top: 58px;
+  max-height: 300px;
+  overflow-y: auto;
+  z-index: 999;
+  flex-wrap: wrap;
+  &::-webkit-scrollbar{
+   width: 5px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: grey;
+    border-radius: 10px;
+  }
+  background-color: var(--theme-primary);
+  ${Wrapper}:hover & {
+    display: flex;
+  }
+`
+
+export const DataWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 100%;
+  font-size: 14px;
+`
+
+export const Name = styled.span`
+  width: 37.5%;
+`
+
+export const Change = styled.span`
+ width: 25%;
+ text-align: center;
+`
+
+export const Price = styled.span`
+ width: 37.5%;
+ text-align: right;
 `
